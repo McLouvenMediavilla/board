be73f657f0edcb5e78e737bb740824e3ffcde922
McLouvenMediavilla <m.mediavilla@klab.com>
2014-02-04 10:36:27 +0800

    First commit on board

---
+++ app/models/thread.php
@@ -0,0 +1,53 @@
+<?php
+class Thread extends AppModel
+{
+	public static function get($id)
+	{
+		$db = DB::conn();
+		$row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
+		return new self($row);
+	}
+
+	public static function getAll()
+	{
+		$threads = array();
+
+		$db = DB::conn();
+		$rows = $db->rows('SELECT * FROM thread');
+		foreach ($rows as $row) {
+			$threads[] = new Thread($row);
+		}
+
+		return $threads;
+	}
+
+	public function getComments()
+	{
+		$comments = array();
+
+		$db = DB::conn();
+		$rows = $db->rows(
+			'SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC',
+			array($this->id)
+		);
+		foreach ($rows as $row) {
+			$comments[] = new Comment($row);
+		}
+
+		return $comments;
+	}
+
+	public function write(Comment $comment)
+	{
+		if (!$comment->validate()) {
+			throw new ValidationException('invalid comment');
+		}
+		
+		$db = DB::conn();
+		$db->query(
+			'INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()',
+			array($this->id, $comment->username, $comment->body)
+		);
+	}
+}
+?>
